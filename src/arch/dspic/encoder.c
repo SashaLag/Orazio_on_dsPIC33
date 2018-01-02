ff1e6a8a29ea2bf691dc3c189c34fa6b8566386a
Sasha <34184409+SashaLag@users.noreply.github.com>
2018-01-02 18:34:48 +0100

    Some minor modifications done for encoder and delay code functions.

--- src/arch/dspic/encoder.c
+++ src/arch/dspic/encoder.c
@@ -1,19 +1,21 @@
+#include <dsp.h>
 #include "encoder.h"
 #include "p33FJ128MC802.h"
 
 #define NUM_ENCODERS 2
-int EncodersValue[2] = {0,0};
+uint16_t EncodersValue[NUM_ENCODERS] = { 0 };
 
 // initializes the encoder subsystem
 void Encoder_init(void){
-  AD1PCFGL |= 0x0038;      // Configure QEI pins as digital inputs
+  AD1PCFGL |= 0x0038;     // Configure QEI pins as digital inputs
   QEICONbits.CNTERR = 0;  // Clear any count errors
   QEICONbits.QEISIDL = 0; // Continue operation during sleep
   QEICONbits.QEIM = 0;    // Disable QEI Module
   QEICONbits.SWPAB = 0;   // QEA and QEB not swapped
   QEICONbits.PCDOUT = 0;  // Normal I/O pin operation
   QEICONbits.POSRES = 1;  // Index pulse resets position counter
-  POSCNT = 0; // Reset position counter
+  POS1CNT = 0;            // Reset position first counter
+  POS2CNT = 0;            // Reset position second counter
   DFLTCONbits.CEID = 1;   // Count error interrupts disabled
   DFLTCONbits.QEOUT = 1;  // Digital filters output enabled for QEn pins
   DFLTCONbits.QECK = 5;   // 1:64 clock divide for digital filter for QEn
@@ -22,8 +24,10 @@ void Encoder_init(void){
 
 // samples the encoders, saving the respective values in a temporary storage
 void Encoder_sample(void){
-  EncodersValue[1]=POS1CNT;
-  EncodersValue[2]=POS2CNT;
+  asm volatile ("disi #0x3FFF");  // disable all user interrupts (atomically)
+    EncodersValue[0]=POS1CNT;
+    EncodersValue[1]=POS2CNT;
+  asm volatile ("disi #0");       //enable all user interrupts (atomically)
 }
 
 // returns the number of the encoder
